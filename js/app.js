72cac72b1d042f6500f77dad22280e7a795873f1
Emin.exe <75854763+eminexedev@users.noreply.github.com>
2022-05-06 01:21:16 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const header = document.querySelector('header');
+const section = document.querySelector('section');
+
+
+function stickyNavbar() {
+    header.classList.toggle("scrolled", window.pageYOffset > 0);
+}
+
+stickyNavbar();
+
+window.addEventListener('scroll', stickyNavbar);
+
+
+// reveal
+
+let sr = ScrollReveal({
+    duration: 2500,
+    distance: "60px",
+    origin: "bottom",
+});
+
+sr.reveal(".info");
+sr.reveal(".image");

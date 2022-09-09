eb10286e015b1730fe99aa00645320d81248c405
Emin.exe <75854763+eminexedev@users.noreply.github.com>
2022-09-09 18:52:38 +0300

    Update lanyard.js

--- js/lanyard.js
+++ js/lanyard.js
@@ -44,13 +44,13 @@ function update_presence() {
     }
 
     if (api.d.discord_status === "dnd") {
-        statusContent.innerHTML = `<span class="w-3 h-3 bg-red-500 rounded-full inline-flex ml-1 mr-2"></span>Do not Disturb`;
+        statusContent.innerHTML = `<span class="w-3 h-3 bg-red-500 rounded-full inline-flex ml-1 mr-2"></span>Do not Disturb - on discord status`;
 
     } else if (api.d.discord_status === "idle") {
-        statusContent.innerHTML = `<span class="w-3 h-3 bg-yellow-500 rounded-full inline-flex ml-1 mr-2"></span>Idle`;
+        statusContent.innerHTML = `<span class="w-3 h-3 bg-yellow-500 rounded-full inline-flex ml-1 mr-2"></span>Idle - on discord status`;
 
     } else if (api.d.discord_status === "online") {
-        statusContent.innerHTML = `<span class="w-3 h-3 bg-green-500 rounded-full inline-flex ml-1 mr-2"></span>Online`;
+        statusContent.innerHTML = `<span class="w-3 h-3 bg-green-500 rounded-full inline-flex ml-1 mr-2"></span>Online - on discord status`;
 
     } else if (api.d.discord_status === "offline") {
         statusContent.innerHTML = `<span class="w-3 h-3 bg-gray-500 rounded-full inline-flex ml-1 mr-2"></span>Offline - on discord status `;

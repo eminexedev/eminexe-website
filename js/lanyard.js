0197f14e1eed9c54e2bc850f2aa581daeaf81d7b
Emin.exe <75854763+eminexedev@users.noreply.github.com>
2023-02-21 22:20:30 +0300

    Update lanyard.js

--- js/lanyard.js
+++ js/lanyard.js
@@ -63,12 +63,6 @@ function update_presence() {
     }
 
     if (api.d.listening_to_spotify === true) {
-        spotfystat.innerHTML = `<span><p> Listening to Spotify </p><p> ${api.d.spotify.song} </p><p> ${api.d.spotify.artist} </p></span>`;
-    } else {
-        spotfystat.innerHTML = `<span><p>I don't listen to Spotify.</p></span> `;
-    }
-
-    if (api.d.listening_to_spotify === true) {
         spotfystat.innerHTML = `<span> <p> <i class="fa-brands fa-spotify"></i> Listening to Spotify </p>  <p> Song: ${api.d.spotify.song} </p> <p> Artist: ${api.d.spotify.artist} </p></span>`;
     } else {
         spotfystat.innerHTML = `<span><p> <i class="fa-brands fa-spotify"></i> I don't listen to Spotify.</p></span> `;
